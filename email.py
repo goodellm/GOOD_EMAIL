91e8f1c064ead1b9489c13541ac75bc3ce811b6c
goodellm <goodellm@alum.mit.edu>
2023-06-16 13:09:24 -0400

    Update for python3 and separate resource files
    Also add main and update reference link

--- email.py
+++ email.py
@@ -1,69 +1,90 @@
 #Email Subject Line Creator
-# based on <www.clickhole.com/article/tips-crafting-perfect-email-subject-line-1729>
+# based on <https://clickhole.com/tips-for-crafting-the-perfect-email-subject-line-1825120753/>
+
 # sea life facts are mostly from:
 #   <seawifs.gsfc.nasa.gov/OCEAN_PLANET/HTML/education_marine_life_factsheet.html>
 #   <http://www.uwphotographyguide.com/10-underwater-creature-facts>
+
+from __future__ import print_function
+
 import random
+import os
 
-seaLifeFacts = ['The blue whale is the largest animal known to have ever lived. ', 'Hydrothermal vents in the sea floor allow ecosystems to exist without energy from the sun. ', 'The oarfish is the longest bony fish in the world. ', 'Green turtles can migrate more than 1,400 miles to lay their eggs. ', 'A group of herring is called a seige. ', 'A group of jellyfish is called a smack. ', 'Bluefin tuna are among the largest and fastest of marine fish. ', 'Penguins "fly" underwater at speeds of up to 25mph. ', 'Horseshoe crabs have existed in essentially the same form for the past 135 million years. ','Many bony fish have more than just one set of nostrils. ','Not all Hermit Crabs use discarded seashells as their portable shelters. ','Sharks are covered with tiny little teeth called dermal denticles. ','Damselfish are farmers growing little algae gardens. ','Moray Eels open and close their mouths, not as an aggressive behavior but as part of their respiration process.','At night Parrotfish enclose themselves in a bubble of their own mucus to avoid being smelled by predators. ','Many species of fish are hermaphrodites. ','Barnacles are actually crustaceans related to crabs and lobsters. ']
-beautifulWords = ['mellifluous ', 'nefarious ', 'epoch ', 'ethereal ', 'iridescent ', 'syzygy ', 'effervescence ', 'turbulence ', 'assemblage ', 'becoming ', 'dalliance ', 'elixir ', 'halcyon ', 'labyrinthine ','eudaimonia ','ephemeral ','coruscating ','quantum ','equilibrium ','email ','pheasant ','downpour ','schist ','geological ', 'enzyme ','xylem ','flourish ', 'swashbuckling']
+def main():
+    sea_life_facts=[]
+    beautiful_words=[]
+    # locate resources regardless of directory from which this is called
+    script_dir=os.path.dirname(os.path.abspath(__file__))
+    resources=os.path.join(script_dir,"resources")
 
-print "EMAIL SUBJECT LINE CREATOR EXTRAORDINAIRE"
+    # load resources from files
+    with open(os.path.join(resources,"sea_life_facts.txt"),"r") as f:
+        contents=f.read()
+        sea_life_facts=[item for item in contents.split("\n") if (item!="")]
+    with open(os.path.join(resources,"beautiful_words.txt"),"r") as f:
+        contents=f.read()
+        beautiful_words=[item for item in contents.split("\n") if (item!="")]
 
-#Start off with GOOD EMAIL
-subj = "GOOD EMAIL "
+    print("EMAIL SUBJECT LINE CREATOR EXTRAORDINAIRE")
 
-# add upsidedown question mark
-subj = subj+u'\u00BF '
+    #Start off with GOOD EMAIL
+    subj = "GOOD EMAIL "
 
-# add sea life fact if boss
-ans = raw_input("Is the recipient of this email your boss? (y/n) ")
-r = 0 # r actuall = random number within size of slf
-if ans[0].lower() == 'y':
-    r=random.randint(0,len(seaLifeFacts)-1)
-    subj = subj + seaLifeFacts[r]
-    
-# add aaaaa
-subj = subj + "aaaaa "
+    # add upsidedown question mark
+    subj = subj+u'\u00BF '
 
-#add a beautiful word
-r=random.randint(0,len(beautifulWords)-1)
-subj = subj + beautifulWords[r]
-    
-# don't take a stance on Jeremy Piven either way
-ans = raw_input("Would you like to take a stance on Jeremy Piven? ")
-if ans[0].lower() == 'y':
-    print "Sorry, this is a poor choice for an email subject line."
-else:
-    print "Good choice."
-    
-#add Regarding The Position
-subj = subj + "Regarding The Position "
+    # add sea life fact if boss
+    ans = input("Is the recipient of this email your boss? (y/n) ")
+    r = 0 # r actuall = random number within size of slf
+    if ans[0].lower() == 'y':
+        r=random.randint(0,len(sea_life_facts)-1)
+        subj = subj + sea_life_facts[r] + " "
+        
+    # add aaaaa
+    subj = subj + "aaaaa "
 
-#add a beautiful word
-r=random.randint(0,len(beautifulWords)-1)
-subj = subj + beautifulWords[r]
-
-# I HAVE YOUR DAUGHTER / YOU HAVE A DAUGHTER if urgent
-ans = raw_input("Is the email urgent? ")
-if ans[0].lower() == 'y':
-    daughter = raw_input("Does the recipient have a daughter? ")
-    if daughter.lower() == 'y':
-        subj = subj + "I HAVE YOUR DAUGHTER "
-    else:
-        subj = subj + "YOU HAVE A DAUGHTER "
-       
-# add I Love You if indicated
-ans = raw_input("Do you love the recipient of this email? ")
-if ans[0].lower() == 'y':
     #add a beautiful word
-    r=random.randint(0,len(beautifulWords)-1)
-    subj = subj + beautifulWords[r]
-    subj = subj + "I love you "
+    r=random.randint(0,len(beautiful_words)-1)
+    subj = subj + beautiful_words[r] + " "
+        
+    # don't take a stance on Jeremy Piven either way
+    ans = input("Would you like to take a stance on Jeremy Piven? ")
+    if ans[0].lower() == 'y':
+        print("Sorry, this is a poor choice for an email subject line.")
+    else:
+        print("Good choice.")
+        
+    #add Regarding The Position
+    subj = subj + "Regarding The Position "
 
-# add email recipient's address at the end
-email = raw_input("What is the email address of the recipient? ")
-subj = subj + email
+    #add a beautiful word
+    r=random.randint(0,len(beautiful_words)-1)
+    subj = subj + beautiful_words[r] + " "
 
-print "\nYour subject line:"
-print subj
+    # I HAVE YOUR DAUGHTER / YOU HAVE A DAUGHTER if urgent
+    ans = input("Is the email urgent? ")
+    if ans[0].lower() == 'y':
+        daughter = input("Does the recipient have a daughter? ")
+        if daughter.lower() == 'y':
+            subj = subj + "I HAVE YOUR DAUGHTER "
+        else:
+            subj = subj + "YOU HAVE A DAUGHTER "
+        
+    # add I love you if indicated
+    ans = input("Do you love the recipient of this email? ")
+    if ans[0].lower() == 'y':
+        #add a beautiful word
+        r=random.randint(0,len(beautiful_words)-1)
+        subj = subj + beautiful_words[r] + " "
+        subj = subj + "I love you "
+
+    # add email recipient's address at the end
+    email = input("What is the email address of the recipient? ")
+    subj = subj + email
+
+    print("\nYour subject line:")
+    print(subj)
+
+
+if __name__ == "__main__":
+    main()
